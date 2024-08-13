71caa8b51d8e63a0e05ae2d4f85e060b1c8f7dec
Toilet Gaming <yusheng6969.com>
2024-08-13 16:43:58 +0800

    done

--- index.php
+++ index.php
@@ -10,109 +10,74 @@ include 'component/header.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Final Project</title>
     <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-	<script src="https://kit.fontawesome.com/1eeb591e79.js" crossorigin="anonymous"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 	
   <!-- <div style="margin-left: 75%;font-size:20px;">
 			<a href="https://maps.app.goo.gl/NNuatMps4J5WhQn38" style="text-decoration: none;"><i class="fa-solid fa-location-dot"></i> Location</a>
 		</div> -->
 </head>
 
-<body class="container fluid">
+<body>
 	<br><br>
-  <div id="carouselExampleCaptions" class="carousel slide" style="height: 500px;" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  <div class="container fluid">
+    <div id="carouselExampleCaptions" class="carousel slide" style="height: 500px;" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="fp/大船/387030793_10220239429290816_125265003866104975_n.jpg" style="height: 500px;" class=" w-100" alt="...">
+        <div class="carousel-caption d-none d-md">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="fp/大船/387030793_10220239429290816_125265003866104975_n.jpg" style="height: 500px;" class=" w-100" alt="...">
+        <div class="carousel-caption d-none d-md">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the second slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="fp/大船/387120463_10220239428890806_7036265817091873135_n.jpg" style="height: 500px;" class=" w-100" alt="...">
+        <div class="carousel-caption d-none d-md">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="fp/大船/387030793_10220239429290816_125265003866104975_n.jpg" style="height: 500px;" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="fp/大船/387030793_10220239429290816_125265003866104975_n.jpg" style="height: 500px;" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="fp/大船/387120463_10220239428890806_7036265817091873135_n.jpg" style="height: 500px;" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
+    <br><br>
+    <div class="container my-4">
+        <div class="row g-3 text-center">
+            <div class="col-12 col-md-6 col-lg-3">
+                <button class="btn btn-secondary w-100" style="background-color: #EF9C9C; padding: 20px;">About Us</button>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <button class="btn btn-secondary w-100" style="background-color: #88B9E6; padding: 20px;">Category</button>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <button class="btn btn-secondary w-100" style="background-color: #8DDF8C; padding: 20px;">Contact Us</button>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <button class="btn btn-secondary w-100" style="background-color: #E6DA70; padding: 20px;">Product List</button>
+            </div>
+        </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
 
-	<!-- <div class="w3-content w3-section" style="max-width:500px;margin-left: 6%;">
-		<img class="mySlides"  src="fp/大船/387030793_10220239429290816_125265003866104975_n.jpg" style="width:1300px;height:550px;">
-		<img class="mySlides" src="fp/大船/387828897_10220239428490796_4252796094018347937_n.jpg" style="width:1300px;height:550px;">
-		<img class="mySlides"  src="fp/大船/387120463_10220239428890806_7036265817091873135_n.jpg" style="width:1300px;height:550px;">
-	</div>
-  <div style="text-align:center">
-    <span class="dot" onclick="currentSlide(1)"></span>
-    <span class="dot" onclick="currentSlide(2)"></span>
-    <span class="dot" onclick="currentSlide(3)"></span>
-  </div> -->
-  <br><br>
-  <div class="line">
-    <button class="line_category">Category</button>
-    <button class="line_contact">Contact Us</button>
-    <button class="line_product">Product List</button>
-    <button class="line_product">Product List</button>
-  </div>
 </body>
 </html>
-
-<script>
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2500); // Change image every 2 seconds
-}
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-</script>
