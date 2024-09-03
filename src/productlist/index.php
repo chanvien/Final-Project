431456f1741f66dff397c6162bdfff241d066d5d
Toilet Gaming <yusheng6969.com>
2024-09-03 15:26:40 +0800

    admin page lmao

--- src/productlist/index.php
+++ src/productlist/index.php
@@ -102,11 +102,7 @@ if (isset($_GET['id']) && isset($_GET['action']) && $_GET['action'] == "cart") {
                 </div>
             </div>
         </div>
-        <?php }}?>     
-
-      
-
-    
+        <?php }}?>
     </div>
 </div>
 
